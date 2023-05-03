a95d0957d7fa028abd4e30cd7a0995af4b74dcdb
SARAVANAN R <108886356+23121991@users.noreply.github.com>
2023-05-04 02:42:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,64 @@
-var res = fetch("https://restcountries.com/v3.1/all");
-res.then((data) => data.json())
-  .then((data1) => foo(data1));
-function foo(data1) {
-  for (var i = 0; i <= data1.length; i++) {
-    console.log(data1[i]);
-    var div = document.createElement("div");
-  //   // div.innerhtml =
-      // array.forEach(element) => {
-      //   console.log(i,data1[i]);
-      //   var div= document.createElement("div");
-        div.setAttribute("class","col-lg-3","col-md-3","col-sm-3");
-div.innerHTML =`<div class="box">
-<div class ="container">
-  <div class="row">
-  <div class="col-lg-3","col-md-3","col-sm-3">
-  <div class= "box">
-    <div class="card group">
-    <div class="card">
-      <div class="card-body">
-      <div><img src= ${data1[i].flags.svg} height=150px width=150px "alt="...";></div>
-       <h4 class="card title">${data1[i].name.common}</h4>
-         <h5 class="region">Region:${data1[i].region}</h5>
-          <h6 class="capital">Capital:${data1[i].capital}</h6>
-            <p class="latitude">Latitude:${data1[i].latlng[0]};</p>
-              <p class="longitude">Longitude:${data1[i].latlng[1]}</p>
-               <p="countrycode">Country Code:${data1[i].cca3}</p>
-                 <button type="click" "btn btn-primary">
-                 <a href="https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=dea32e72b6b3bf87cb81963918204b5f">click for weather</a> </button>
-            </div>
+async function api(){
+  try{
+  data = fetch('https://restcountries.com/v3.1/all')
+    out = await data;
+     prom = out.json();
+     final = await prom;
+     parent  = document.querySelector('.container')
+    parent1 = document.querySelector('.row')
+     final.forEach(element => {
+
+   
+     parent1.innerHTML+=`
+          <div id="cardDetails" class="col-xl-4 col-lg-4 col-md-4 col-sm-6">
+          <div class="card h-100">
+           <div class="card-header">
+           <h5 class="card-title">${element.name.common}</h5>
+          </div><br>
+          <div class="card-body">  
+          <img src="${element.flags.png}" class="card-img-top">
+         <div class="card-text">
+         <ul class="list-group">
+         <li class="list-group-item card-text"><b>Capital:${element.capital}</li>
+         <li class="list-group-item card-text"><b>Region:${element.region}</li>
+         <li class="list-group-item card-text"><b>Country Code:${element.cca3}</li>
+         
+       </div>
+        
+
+         <button class="btn btn-primary" target="_blank" value="${element.name.common}">Click for Weather</button>
+
           </div>
-          
         </div>
-        </div>
-       </div>
-       </div>
-     </div>`
-     document.body.append(div);
-       
-      };  
-    
-    }
+      
+      `
+      parent.append(parent1)
+      
+      let btn = document.querySelectorAll(".btn");
+      btn.forEach((ele)=>{
+        ele.addEventListener("click",()=>{
+          let value = ele.value
+          console.log(value)
+          async function weather(){
+            let res = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ele.value}&APPID=92e3d7890b585097988ec7b296561184`)
+            let ress = await res
+            let res11 = ress.json()
+            let res1 = await res11
+
+            console.log(res1)
+            ele.innerHTML= `weather: ${res1.weather[0].description}<br>Temp: ${res1.main.temp}<br>Pressure: ${res1.main.pressure}<br>
+            lon:${res1.coord.lon}<br>lat${res1.coord.lat}`
            
+          }
+          weather();
+        })
+      })
+      
+     });
+    }
+     catch(error){
+      console.log(error)
+     }
+    
+}
+api()
